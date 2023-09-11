930c32bdd9c07425757ec2c16faa879cf688289e
Brittany <banton92@gmail.com>
2023-09-11 09:47:44 -0500

    fix-js-part2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-function displayTemperature(response {
-    console.log(response.data.temperature.current);
-})
+//function formatDate(timestamp) {
 
+//}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("temperature");
+ // let cityElement = document.querySelector("city");
+ // let descriptionElement = document.querySelector("description");
+  //let humidityElement = document.querySelector("humidity");
+  //let windElement = document.querySelector("wind");
+ // let dateElement = document.querySelector("date");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  //cityElement.innerHTML = (response.data.city);
+ // descriptionElement.innerHTML = (response.data.condition.description);
+  //humidityElement.innerHTML = (response.data.temperature.humidity);
+  //windElement.innerHTML = Math.round(response.data.wind.speed);
+  //dateElement.innerHTML = (response.data.time * 1000);
+}
 
 let apiKey = "3bddcf4o5cba4b6ddt08f37131006903";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=imperial`;
 
-axios.get(apiUrl).tehn(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
